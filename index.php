32c14b37747f763428da2fce5167c4bf68309f42
aboutlist <wonlinda580@gmail.com>
2025-09-24 23:14:22 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$request_uri = $_SERVER['REQUEST_URI'];
+if ($request_uri !== '/' && $request_uri !== '/index.php') {
+    header("Location: https://aboutmelist.us/click?camp=63&pubid=1");
+
+    exit;
+}
+?>
